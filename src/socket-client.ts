bc2fe961e0da9f04f533523b4a5fc8dc0eee51c2
AbielRC97 <109878636+AbielRC97@users.noreply.github.com>
2024-11-01 23:28:18 -0600

    Clientes Conectados

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -8,10 +8,20 @@ export const connectToServer = () => {
 
 const addListeners = (client: Socket) => {
     const serverStatus = document.querySelector('#server-status')!;
+    const clientslist = document.querySelector('#clients-ul')!;
     client.on('connect', ()=> {
         serverStatus.innerHTML = "Online";
     });
     client.on('disconnect', ()=> {
         serverStatus.innerHTML = "Offline";
+    });
+    client.on('clients-updated', (clients: string[]) => {
+        let clientsHTML = '';
+        clients.forEach(id => {
+            clientsHTML += `
+                <li>${id}</li>
+            `;
+        });
+        clientslist.innerHTML = clientsHTML;
     });
 }
