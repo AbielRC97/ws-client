b3ea1fc5cecdc9d9f4f134e199325f6d838f552c
AbielRC97 <109878636+AbielRC97@users.noreply.github.com>
2024-11-01 23:12:51 -0600

    Asignar Status

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import './style.css'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>  Websocket - client</h1>
+    <span id="server-status">offline</span>
   </div>
 `
 
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,7 +1,17 @@
-import { Manager } from "socket.io-client"
+import { Manager, Socket } from "socket.io-client"
 
 export const connectToServer = () => {
     const maneger = new Manager(`http://localhost:3000/socket.io/socket.io.js`);
     const socket = maneger.socket('/');
-    
+    addListeners(socket);
+}
+
+const addListeners = (client: Socket) => {
+    const serverStatus = document.querySelector('#server-status')!;
+    client.on('connect', ()=> {
+        serverStatus.innerHTML = "Online";
+    });
+    client.on('disconnect', ()=> {
+        serverStatus.innerHTML = "Offline";
+    });
 }
